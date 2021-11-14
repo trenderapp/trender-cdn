c70c3d8fc965140da74dbf439f75d79992dbf7d3
alex_0ff <coulay37@gmail.com>
2021-11-14 22:07:08 +0100

    Adding files

---
+++ src/Pages/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Access denied</h1>
+        </div>
+    )
+}
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Switch />
+                <Route exact path="/" component={Home} />
+            <Switch />
+        </BrowserRouter>
+
+    );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./Pages/App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
